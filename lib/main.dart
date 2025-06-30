511e4d3c9491753151329c2e21994b94837c781c
ParthKulkarni445 <2023csb1142@gmail.com>
2025-06-30 16:57:33 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  
+
   // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -23,22 +23,21 @@ void main() async {
 
   runApp(
     MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => UserProvider()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          textSelectionTheme: const TextSelectionThemeData(
-            selectionHandleColor: Colors.blue,
-            cursorColor: Colors.blue,
+        providers: [
+          ChangeNotifierProvider(create: (_) => UserProvider()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            textSelectionTheme: const TextSelectionThemeData(
+              selectionHandleColor: Colors.blue,
+              cursorColor: Colors.blue,
+            ),
+            fontFamily: 'Poppins',
+            primaryColor: Colors.white,
           ),
-          fontFamily: 'Poppins',
-          primaryColor: Colors.white,
-        ),
-        home: const MyApp(),
-      )
-    ),
+          home: const MyApp(),
+        )),
   );
 }
 
@@ -59,14 +58,14 @@ class _MyAppState extends State<MyApp> {
     initialise();
   }
 
-  void initialise() async{
+  void initialise() async {
     await authService.getUserData(context);
     FlutterNativeSplash.remove();
   }
 
   @override
   Widget build(BuildContext context) {
-    if(Provider.of<UserProvider>(context).user.token.isEmpty){
+    if (Provider.of<UserProvider>(context).user.token.isEmpty) {
       return const AuthPage();
     } else {
       return const LandingPage();
@@ -75,43 +74,41 @@ class _MyAppState extends State<MyApp> {
 }
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-    // This handler will be called when the app is in the background or terminated
-    print("Handling a background message: ${message.messageId}");
-    print("Notification Title: ${message.notification?.title}");
-    print("Notification Body: ${message.notification?.body}");
+  // This handler will be called when the app is in the background or terminated
+  print("Handling a background message: ${message.messageId}");
+  print("Notification Title: ${message.notification?.title}");
+  print("Notification Body: ${message.notification?.body}");
 }
 
 class NotificationService {
-    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
-    initFCM (BuildContext context)async{
-        await _firebaseMessaging.requestPermission();
+  initFCM(BuildContext context) async {
+    await _firebaseMessaging.requestPermission();
 
-        String? token = await _firebaseMessaging.getToken();
-        if (token != null) {
-            print("FCM Token: $token");
-        } else {
-            print("Failed to get FCM token");
-        }
-
-        await _firebaseMessaging.subscribeToTopic('contest-reminders');
-
-        FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-            print("onMessage: ${message.notification?.title} - ${message.notification?.body}");
-        });
-
-        FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-            print("onMessageOpenedApp: ${message.notification?.title} - ${message.notification?.body}");
-            final action = message.data['action'];
-            if(action == "VIEW_CONTEST") {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ContestsPage(),
-                    ),
-                );
-            }
-        });
-
+    String? token = await _firebaseMessaging.getToken();
+    if (token != null) {
+      print("FCM Token: $token");
+    } else {
+      print("Failed to get FCM token");
     }
+
+    await _firebaseMessaging.subscribeToTopic('contest-reminders');
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print(
+          "onMessage: ${message.notification?.title} - ${message.notification?.body}");
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print(
+          "onMessageOpenedApp: ${message.notification?.title} - ${message.notification?.body}");
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ContestsPage(),
+        ),
+      );
+    });
+  }
 }
